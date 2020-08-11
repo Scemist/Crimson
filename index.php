945ec1654635e9147fb93c1477b8cfe66887d2ef
Lucas Gonçalves S <lucas.g.custer@gmail.com>
2020-08-11 01:31:49 -0300

    Primeiro upload
    Os arquivos Bootstrap e index.php, primeiras pastas também.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"> <!-- 4.1.3 -->
+
+    <title>Crimson Vinhos</title>
+  </head>
+
+  <body>
+
+    <main class="container">
+      <h1 class="display-4 text-center">Bem vindo a um mar vermelho crimson</h1>
+    </main>
+
+    <script src="bootstrap/jquery/jquery-3.3.1.slim.min.js"></script> <!-- jQuery -->
+    <script src="bootstrap/popper/popper.min.js"></script> <!-- Popper.js -->
+    <script src="bootstrap/js/bootstrap.min.js"></script> <!-- Plugins JavaScript -->
+  </body>
+</html>
