5281b878dd4b926ead5e97cb5b752c6e3cc737fc
Lucas Gonçalves S <lucas.g.custer@gmail.com>
2020-08-11 04:04:07 -0300

    Criação de "caracteristicas" e "tipos"
    A criação dessas duas páginas, sem conteúdo, a não ser por um template. Adição de imagens na index.

--- index.php
+++ index.php
@@ -17,14 +17,57 @@
       <div class="row">
         <div class="col-12">
           <a href="index.php">
-            <h3 class="text-center corCreme fonteOSlight my-2">Crimson</h3>
+            <h2 class="text-center corCreme fonteOSlight my-3">Crimson</h2>
           </a>
         </div>
       </div>
     </header>
 
     <main class="container">
-      <h1 class="display-4 text-center corAzul1 fonteZSlight">Bem vindo a um mar vermelho crimson</h1>
+      <div class="row">
+        <div class="col-12">
+          <p class="fonteOSlight text-center text-muted mt-5">Crimson Vinhos</p>
+          <h4 class="text-center corAzul1 fonteZSlight mb-5">Seja bem vindo a um mar vermelho crimson. Um site inteiramente dedicado a vinhos.</h4>
+          <hr>
+        </div>
+
+        <div class="col-12 col-md-4 px-0">
+          <img src="css/imagens/vinho01.jpg" class="img-fluid mx-auto d-block" alt=". . .">
+        </div>
+        <div class="col-12 col-md-4 px-0">
+          <img src="css/imagens/vinho01.jpg" class="img-fluid mx-auto d-block" alt=". . .">
+        </div>
+        <div class="col-12 col-md-4 px-0">
+          <img src="css/imagens/vinho01.jpg" class="img-fluid mx-auto d-block" alt=". . .">
+        </div>
+
+        <div class="col-12">
+          <p class="fonteOSlight text-center text-muted mt-5 mb-2">Parâmetros do vinho</p>
+          <p class="fonteZSlight text-center h4 mb-5">Há duas classes principais para se avaliar um vinho:</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-10 col-md-3 mx-auto">
+          <a href="caracteristicas.php">
+            <button class="botao py-3 w-100 corAzul1 fonteOSlight">SUAS CARACTERÍSTICAS</button>
+          </a>
+        </div>
+      </div>
+
+      <div class="row my-3">
+        <div class="col-10 col-md-3 mx-auto">
+          <a href="tipos.php">
+            <button href="#" class="botao py-3 w-100 corAzul1 fonteOSlight">SEUS TIPOS</button>
+          </a>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12">
+          <hr>
+        </div>
+      </div>
     </main>
 
     <script src="bootstrap/jquery/jquery-3.3.1.slim.min.js"></script> <!-- jQuery -->
