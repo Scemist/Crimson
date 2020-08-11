9da4a92587a41a8d19ec84a665d0b81e10e988a9
Lucas Gonçalves S <lucas.g.custer@gmail.com>
2020-08-11 02:37:27 -0300

    Adição de fontes e imagem de fundo
    As fontes Open Sans e ZillaSlab foram adicionadas aos arquivos e ao CSS. Uma imagem com textura para o fundo também.

--- index.php
+++ index.php
@@ -3,16 +3,28 @@
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"> <!-- 4.1.3 -->
+    <link rel="stylesheet" href="css/geral.css">
 
     <title>Crimson Vinhos</title>
   </head>
 
-  <body>
+  <body class="fundo">
+
+    <header class="container-fluid corAzul1Fundo">
+      <div class="row">
+        <div class="col-12">
+          <a href="index.php">
+            <h3 class="text-center corCreme fonteOSlight my-2">Crimson</h3>
+          </a>
+        </div>
+      </div>
+    </header>
 
     <main class="container">
-      <h1 class="display-4 text-center">Bem vindo a um mar vermelho crimson</h1>
+      <h1 class="display-4 text-center corAzul1 fonteZSlight">Bem vindo a um mar vermelho crimson</h1>
     </main>
 
     <script src="bootstrap/jquery/jquery-3.3.1.slim.min.js"></script> <!-- jQuery -->
