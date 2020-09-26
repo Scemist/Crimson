27294abf1d200618cc000d656f62085634a8775b
Lucas Gonçalves S <scemist.lucas@gmail.com>
2020-09-26 06:14:58 -0300

    Passagem da Index para Flex
    Toda a página Index está como Flex agora, os botões também têm classes CSS.

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
 
 			<section>
 				<div>
-					<header>
+					<header class="blocoMaior">
 						<p>Crimson Vinhos</p>
 						<h4 class="corAzul1 destaque">Seja bem vinda(o) a um mar vermelho crimson, um site inteiramente dedicado a vinhos.</h4>
 						<hr>
@@ -64,101 +64,100 @@
 						<img src="imagens/vinho03.jpg" alt=". . .">
 					</article>
 
-					<div>
+					<div class="blocoMenor">
 						<p>
 							Todo o site foi inteiramente projetado de modo único e direto, pensado com o objetivo de ensinar toda a base sobre vinhos. Com isso você vai entender completamente tudo o que é preciso, e então ficará livre para descobrir conhecimentos específicos pelo mundo em cima disso.
 						</p>
 					</div>
 
-					<div>
+					<div class="blocoMenor">
 						<hr>
 						<p>
 							Nós dividimos a classificação do vinho em três, basicamnte você sempre terá três pontos a considerar de um vinho: tipos; adjetivos e características.
 						</p>
 
-						<ul>
-							<li class="listaLi">Todo vinho está em um dos três tipos existentes.</li>
-							<li class="listaLi">Um vinho pode ter um ou mais adjetivos. </li>
-							<li class="listaLi">Todo vinho tem as mesmas cinco características, em quantidades diferentes.</li>
-						</ul>
+						<p>
+							Todo vinho está em um dos três tipos existentes, um vinho pode ter um ou mais adjetivos e todo vinho tem as mesmas cinco características, em quantidades diferentes.
+						</p>
 
 						<hr>
+
 						<p>
 							Então aqui está a lista das páginas para estes três pontos, leia os três, e quando terminas, bem, isso é tudo. No fim desta página está um sumário com tudo, assim você sempre poderá voltar no site para servir como consulta:
 						</p>
 					</div>
 
-					<div>
+					<div class="botoes">
 						<a href="caracteristicas.php">
-							<button class="botao corAzul1 fonteOSregular">SEUS TIPOS</button>
+							<button class="botao">SEUS TIPOS</button>
 						</a>
 
 						<a href="caracteristicas.php">
-							<button class="botao corAzul1 fonteOSregular">SEUS ADJETIVOS</button>
+							<button class="botao">SEUS ADJETIVOS</button>
 						</a>
 
 						<a href="caracteristicas.php">
-							<button class="botao corAzul1">SUAS CARACTERÍSTICAS</button>
+							<button class="botao">SUAS CARACTERÍSTICAS</button>
 						</a>
 					</div>
 				</div>
 
+				<div class="">
+					<hr>
+					<p>SUMÁRIO COMPLETO DO SITE</p>
+				</div>
+				
 				<div class="sumario">
-					<div>
-						<hr>
-						<p>SUMÁRIO COMPLETO DO SITE</p>
-					</div>
-
-					<div>
+					<div class="lista">
 						<ul>
 							<h3>
-								<a href="#">Tipos</a>
+								<a href="#">TIPOS</a>
 							</h3>
 							<li>
-								<a href="#">TINTO</a>
+								<a href="#">Tinto</a>
 							</li>
 							<li>
-								<a href="#">ROSÉ</a>
+								<a href="#">Rosé</a>
 							</li>
 							<li>
-								<a href="#">BRANCO</a>
+								<a href="#">Branco</a>
 							</li>
 						</ul>
 					</div>
 
-					<div>
+					<div class="lista">
 						<ul>
 							<h3>
-								<a href="#">Adjetivos</a>
+								<a href="#">ADJETIVOS</a>
 							</h3>
 							<li>
-								<a href="#">TRANQUILO</a>
+								<a href="#">Tranquilo</a>
 							</li>
 							<li>
-								<a href="#">ESPUMANTE</a>
+								<a href="#">Espumante</a>
 							</li>
 							<li>
-								<a href="#">FORTIFICADO</a>
+								<a href="#">Fortificado</a>
 							</li>
 							<li>
-								<a href="#">SOBREMESA</a>
+								<a href="#">Sobremesa</a>
 							</li>
 							<li>
-								<a href="#">GENEROSO</a>
+								<a href="#">Generoso</a>
 							</li>
 							<li>
-								<a href="#">VERDE</a>
+								<a href="#">Verde</a>
 							</li>
 							<li>
-								<a href="#">LARANJA</a>
+								<a href="#">Laranja</a>
 							</li>
 							<li>
-								<a href="#">AZUL</a>
+								<a href="#">Azul</a>
 							</li>
 						</ul>
 					</div>
 
-					<div>
+					<div class="lista">
 						<ul>
 							<h3>
 								<a href="#">CARACTERÍSTICAS</a>
