5cd97ff308b091a37539468cf521c3db881d0b33
Lucas Gonçalves S <scemist.lucas@gmail.com>
2020-10-13 21:51:10 -0300

    Correção da nav rolando pela nav
    A nav no celular, apesar de fechar ao tocar fora, ainda rolava por dentro de si, agora o listener é no body, e apenas links salvam.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 const navSlide = () => {
+    
     const nav = window.document.querySelector('.crimson')
     const burger = window.document.querySelector('#burger')
     const logoNav = window.document.querySelector('#logoNav')
     const itensNav = window.document.querySelector('#itensNav')
     const filtro = window.document.querySelector('.escurecer')
     const fechar = window.document.querySelector('.fechar')
+    const body = window.document.querySelector('body')
 
     // Ouve a Nav
     function abrirNav() {
+
         // Expande a Nav
         nav.classList.add('nav-ativa')
-
 
         // Ajustes de estilo
         logoNav.style.animation = `logoFade 0.5 ease forwards 0.5s`
@@ -20,11 +22,10 @@ const navSlide = () => {
         filtro.style.animation = `escurecer 0.5 ease forwards 0.5s`
     }
 
-
     function fecharNav() {
+
         // Retrai a Nav
         nav.classList.remove('nav-ativa') // Retrai a Nav
-
 
         // Ajustes de estilo
         itensNav.style.transform = 'translateX(-100%)'
@@ -38,7 +39,7 @@ const navSlide = () => {
 
     logoNav.addEventListener('click', abrirNav)
     fechar.addEventListener('click', fecharNav)
-    filtro.addEventListener('touchstart', fecharNav)
+    body.addEventListener('touchstart', fecharNav)
 
 }
 
